49dabe5c493974c2cb40a1479a793a4de52e6796
soyeonnnb <soyeonnnb@users.noreply.github.com>
2021-01-25 21:03:39 +0900

    add mvc's view part
    add some view parts

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localMiddleware } from "./middlewares";
 
 const app = express();
 
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
-app.use(helmet());
+app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- routes.js
+++ routes.js
@@ -15,7 +15,7 @@ const CHANGE_PASSWORD = "/change-password";
 // Videos
 
 const VIDEOS = "/videos";
-const UPLOAD = "upload";
+const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
