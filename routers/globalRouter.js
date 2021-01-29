95c44dd7c7391173280640c2ab75bfe179b6a7a6
soyeonnnb <soyeonnnb@users.noreply.github.com>
2021-01-29 17:53:08 +0900

    add upload page
    modified routes. when push submit btn in upload page, go to the video detail page of new videos

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -2,7 +2,8 @@ import express from "express";
 import {
   getJoin,
   postJoin,
-  login,
+  getLogin,
+  postLogin,
   logout,
 } from "../controlloers/userControllers";
 import { home, search } from "../controlloers/videoControllers";
@@ -13,8 +14,11 @@ const globalRouter = express.Router();
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.login, login);
+
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
 
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,19 +1,19 @@
 import express from "express";
-import { videos } from "../controlloers/userControllers";
 import {
   deleteVideo,
   editVideo,
-  search,
-  upload,
+  getUpload,
+  postUpload,
   videoDetail,
 } from "../controlloers/videoControllers";
 import routes from "../routes";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+
+videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
